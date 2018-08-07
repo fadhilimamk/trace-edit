80f9ed836ee3960aa70ae2a70c4d75721ac65691
Fadhil Imam Kurnia <fadhilimamk@gmail.com>
2018-08-07 20:27:52 +0700

    Add new function to handle raid5, add gitignore

--- scripts/filter_raid.py
+++ scripts/filter_raid.py
@@ -68,3 +68,69 @@ def createAllRaidList(infile, ndisk, stripe):
     
 
 
+# Added by Fadhil Imam to create raid 5 tracefile [August 3, 2018]
+
+# Function to create all raid 5 trace files
+#   infile : original tracefile
+#   ndisk : number of all disk for raid 5 configuration (assuming ndisk always >= 3)
+#   segment_size : size of segment in each disk (chunk size), please see : https://4.bp.blogspot.com/-P_0tQLC8lIs/Ucyk5Kpc5yI/AAAAAAAAA7I/ASAMO4y91gA/s795/Storage_segment_and_stripe_size.png
+def createAllRaid5Files(infile, ndisk, segment_size):
+  for i in range (0,ndisk):
+    outfile = open("out/"+infile+"-raid5disk" + str(i) + ".trace", "w")
+    new_trace = createRaid5Subtrace(infile, i, ndisk, segment_size)
+    
+    for x in new_trace:
+      outfile.write(x)
+
+    outfile.close()
+
+
+def createRaid5Subtrace(original_trace, diskid, ndisk, segment_size):
+    out = []
+    blk_size = 4096 # or sector size in hdd (in bytes)
+    blk_per_segment = segment_size / blk_size
+    blk_per_stripe = blk_per_segment * ndisk
+
+    old_trace = open("in/" + original_trace)
+    for line in old_trace:
+      token = line.split(" ")
+      time = token[0]
+      devno = token[1]
+      blkno = int(token[2].strip())
+      blkcount = int(token[3].strip())
+      operation = token[4]
+
+      # calculating new blkno
+      target_stripe_id = blkno / (blk_per_segment * (ndisk-1))
+      blk_stripe_offset = (blkno + (blk_per_segment*target_stripe_id)) % blk_per_stripe
+      parity_disk_id = target_stripe_id % ndisk
+      target_disk_id = blk_stripe_offset / blk_per_segment
+      if parity_disk_id <= target_disk_id:
+        target_disk_id = target_disk_id + 1
+      new_blkno = (target_stripe_id*blk_per_segment) + (blk_stripe_offset%blk_per_segment)
+
+      while True:
+        if target_disk_id == diskid and blkcount != 0:
+          out.append("{} {} {} {} {}".format(time, devno, new_blkno, blkcount, operation))
+
+        # make write commands to parity disk
+        
+        if blkcount <= blk_per_segment:
+          break
+        
+        # need to write to the next segment
+        else:
+          blkcount = blkcount - blk_per_segment
+          target_disk_id = target_disk_id + 1
+          
+          # handle edge of stripe
+          if target_disk_id == ndisk:
+            target_disk_id = 0
+            target_stripe_id = target_stripe_id + 1
+
+          # jump parity disk
+          if target_disk_id == target_stripe_id % ndisk:
+            target_disk_id = target_disk_id + 1
+
+    return out
+
